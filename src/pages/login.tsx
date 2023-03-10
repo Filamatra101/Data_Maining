75d8ee2fd82a6aae35d24a927a90cd0c91d2f502
Filamatra <filamatrarasolofo101@gmail.com>
2023-03-10 08:06:18 +0300

    initialisation

---
+++ src/pages/login.tsx
@@ -0,0 +1,10 @@
+export const Login = (): JSX.Element=>{
+    return(
+        <div>
+            <form>
+                <input type="text" placeholder="Username"/>
+                <input type="password" placeholder="Password"/>
+            </form>
+        </div>
+    )
+}
